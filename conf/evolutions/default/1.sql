a22aa61a007a8f25bbedf8050117604732656e5b
Esdras <servoesdras@gmail.com>
2014-12-09 22:49:55 -0300

    config

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,13 +1,10 @@
-# --- Created by Ebean DDL
-# To stop Ebean DDL generation, remove this comment and start using Evolutions
-
 # --- !Ups
 
 create table alternativa (
   id                        bigint auto_increment not null,
   pergunta_id               bigint not null,
   descricao                 varchar(255),
-  alternativa_certa         tinyint(1) default 0,
+  alternativa_certa         boolean,
   constraint pk_alternativa primary key (id))
 ;
 
@@ -25,6 +22,12 @@ create table resposta (
   nickname_user             varchar(255) not null)
 ;
 
+create sequence alternativa_seq;
+
+create sequence pergunta_seq;
+
+create sequence resposta_seq;
+
 alter table alternativa add constraint fk_alternativa_pergunta_1 foreign key (pergunta_id) references pergunta (id) on delete restrict on update restrict;
 create index ix_alternativa_pergunta_1 on alternativa (pergunta_id);
 alter table resposta add constraint fk_resposta_pergunta_2 foreign key (pergunta_id) references pergunta (id) on delete restrict on update restrict;
@@ -36,13 +39,19 @@ create index ix_resposta_alternativa_3 on resposta (alternativa_id);
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table alternativa;
+drop table if exists alternativa;
 
-drop table pergunta;
+drop table if exists pergunta;
 
-drop table resposta;
+drop table if exists resposta;
 
-SET FOREIGN_KEY_CHECKS=1;
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists alternativa_seq;
+
+drop sequence if exists pergunta_seq;
+
+drop sequence if exists resposta_seq;
 
